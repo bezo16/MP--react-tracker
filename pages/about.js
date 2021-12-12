fd03095169c06e62f936397ac6ad452d51cfbe77
bezo16 <bezo16@protonmail.ch>
2021-12-12 10:18:03 +0100

    init about page , added time in walking

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+
+
+
+
+   export default function About() {
+
+
+        return (
+            <main>
+                <h1>dis is about</h1>
+            </main>
+        )
+   }
--- pages/train.js
+++ pages/train.js
@@ -3,12 +3,14 @@ import styles from '../styles/train.module.css'
 import { useRouter } from "next/router"
 
 
-export default function Home() {
+export default function Train() {
 
     const router = useRouter()
     const [km,setKm] = useState(0)
     const [jumps,setJumps] = useState(0)
     const [height,setHeight] = useState(0)
+
+    const [walkTime,setWalkTime] = useState(0)
 
 
     useEffect(() => {
@@ -16,11 +18,14 @@ export default function Home() {
     },[])
 
 
-    function updateSteps() {
+    function updateWalking() {
         let steps = Math.floor((km * 1000) / ((height * 0.75) / 188))
-        localStorage.setItem('steps',Number(localStorage.getItem('steps')) + Number(steps))
-        setKm(0)
-        router.push('/')
+        if(steps && walkTime) {
+            localStorage.setItem('steps',Number(localStorage.getItem('steps')) + Number(steps))
+            localStorage.setItem('time',Number(localStorage.getItem('time')) + Number(walkTime))
+            setKm(0)
+            router.push('/')
+        }
     }
 
     function updateJumps() {
@@ -35,9 +40,14 @@ export default function Home() {
             <div className={styles.cards}>
                 <div className={styles.card}>
                     <h3 className={styles['card-title']}>Walking</h3>
-                    <label className={styles['card-label']} htmlFor="walking">How much km u hv walked?</label>
-                    <input className={styles['card-input']} id="walking" type="number" value={km} onChange={(e) => setKm(e.target.value)}/>
-                    <button className={styles['card-button']} onClick={updateSteps}>Finish Training</button>
+                    <label className={styles['card-label']} htmlFor="walking-steps">How much km u hv walked?</label>
+                    <input className={styles['card-input']} id="walking-steps" type="number" value={km} onChange={(e) => setKm(e.target.value)}/>
+                    <label className={styles['card-label']} htmlFor="walking-time">How much time did it take?(min)</label>
+                    <input className={styles['card-input']} id="walking-time" type="number" value={walkTime} onChange={(e) => setWalkTime(e.target.value)}/>
+                    <button className={styles['card-button']} onClick={updateWalking}>
+                        Finish Training
+                        <svg fill="var(--orange2)" width="24" height="24" viewBox="0 0 24 24"><path d="M12 18.292v1.834c.644.129 1.177.303 1.496.528.273.192.274.498 0 .69-1.494 1.053-7.498 1.054-8.993 0-.272-.191-.271-.499 0-.69.319-.225.852-.399 1.497-.528v-1.833c-2.363.481-4 1.511-4 2.707 0 1.657 3.134 3 7 3s7-1.343 7-3c0-1.196-1.637-2.226-4-2.708zm7.45-14.94c-2.73 0-2.791-2.352-5.961-2.352-1.382 0-2.679.518-3.489 1.022v-2.022h-2v21h2v-9.122c.934-.553 2.172-1.048 3.5-1.048 2.993 0 3.413 2.237 6.081 2.237 1.058 0 1.914-.415 2.419-.736v-9.779c-.537.342-1.46.8-2.55.8zm.55 4.67c-1.801.486-4.082-1.087-5-1.693v2.671s-1.758-.803-5 .739v-2.901c1.188-1.046 3.292-1.734 5-.508v-2.808c.785.615 2.575 1.737 4.45 1.737.189 0 .372-.012.55-.033v2.796z"/></svg>
+                    </button>
                 </div>
                
             </div>
